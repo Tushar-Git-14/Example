ae2e8c1f4a7cf427ce2cf4a34beb1a0cc2217f1b
Tushar-A <tushar14080@gmail.com>
2026-02-05 13:10:48 +0530

    Add new print statement to sample.java

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ class sample{
    public static void main(String [] args){
 
 		int a = 20 ; int b = 40 ;  int c = a+b;
+	    System.out.println("This is also Newly Added");
 		System.out.println(c);
 }
 }
